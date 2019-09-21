8b6df35c1c2fe077ab7e28e97edc5048d5c575a0
nsanc <n.sanchezesparza@gmail.com>
2019-09-21 15:20:30 -0500

    Creación del proyecto

---
+++ src/main/java/com/springboot/ArticlesmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArticlesmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArticlesmanagerApplication.class, args);
+	}
+
+}
