099367fe5e240aa7ecdc19c51a3cc163327b42e8
nsanc <n.sanchezesparza@gmail.com>
2019-09-21 17:56:44 -0500

    Registrar y visualizar el detalle de un articulo

--- src/main/java/com/springboot/articleapp/controller/ArticleController.java
+++ src/main/java/com/springboot/articleapp/controller/ArticleController.java
@@ -2,10 +2,17 @@ package com.springboot.articleapp.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.springboot.articleapp.model.Article;
 import com.springboot.articleapp.service.ArticleService;
@@ -31,6 +38,38 @@ public class ArticleController {
 		ModelAndView mav = new ModelAndView(INDEX_VIEW);
 		mav.addObject("articles", articles);
 		return mav;
+	}
+
+	@GetMapping("/articles")
+	public ModelAndView getAllArticles() {
+		List<Article> articles = articleService.getAllArticles();
+		ModelAndView mav = new ModelAndView(ARTICLE_PAGE_VIEW);
+		mav.addObject("articles", articles);
+		return mav;
+	}
+
+	@GetMapping("/article/new")
+	public String newArticles(Model model) {
+		model.addAttribute("article", new Article());
+		return ARTICLE_ADD_FORM_VIEW;
+	}
+
+	@PostMapping("/article/create")
+	public String createArticle(@Valid Article article, BindingResult result,RedirectAttributes attr ,Model model) {
+		if (result.hasErrors()) {
+			attr.addFlashAttribute("org.springframework.validation.BindingResult", result);
+			attr.addFlashAttribute("article",article);
+			return "redirect:/article/new";
+		}
+		Article newArticle = articleService.createArticle(article);
+		model.addAttribute("article", newArticle.getArticleId());
+		return "redirect:/article/" + newArticle.getArticleId();
+	}
+
+	@GetMapping("article/{id}")
+	public String getArticleById(@PathVariable(value = "id") Long articleId, Model model) {
+		model.addAttribute("article", articleService.findById(articleId));
+		return ARTICLE_VIEW;
 	}
 
 }
